a03cb70bcb69e01b4f89d5c414d5a83712098e93
IESA Multimedia <IESA Multimedia>
2017-01-10 17:05:35 +0100

    end of the day

---
+++ src/AppBundle/Entity/Tag.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Tag
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\TagRepository")
+ */
+class Tag
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Meuble")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="meuble", referencedColumnName="id")
+     * })
+     */
+    private $meuble;
+
+    /**
+     * @var string     
+     *
+     * @ORM\Column(name="tag", type="string", length=255)
+     */
+    private $tag;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set meuble
+     *
+     * @param \AppBundle\Entity\Meuble $meuble
+     * @return Tag
+     */
+    public function setMeuble($meuble)
+    {
+        $this->meuble = $meuble;
+
+        return $this;
+    }
+
+    /**
+     * Get meuble
+     *
+     * @return \AppBundle\Entity\Meuble
+     */
+    public function getMeuble()
+    {
+        return $this->meuble;
+    }
+
+    /**
+     * Set tag
+     *
+     * @param string $tag
+     * @return Tag
+     */
+    public function setTag($tag)
+    {
+        $this->tag = $tag;
+
+        return $this;
+    }
+
+    /**
+     * Get tag
+     *
+     * @return string 
+     */
+    public function getTag()
+    {
+        return $this->tag;
+    }
+}
