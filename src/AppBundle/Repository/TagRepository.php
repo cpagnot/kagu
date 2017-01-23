db2e3a30f4ff1c5af43a009dccff6ffd655ac32c
cpagnot3 <clementpagnot@gmail.com>
2017-01-23 23:20:10 +0000

    correction

--- src/AppBundle/Repository/TagRepository.php
+++ src/AppBundle/Repository/TagRepository.php
@@ -10,7 +10,7 @@ class TagRepository extends EntityRepository
     public function findBySearch($tags)
     {
     	$sql = "SELECT a.id as id, a.titre as titre, a.description as description, a.photo as photo,
-    	        u.username as pseudo, SUM(m.prix) as prix
+    	        u.username as pseudo, SUM(m.prix) as prix, u.ville as ville
                 FROM c9.ambiance a 
                 LEFT JOIN c9.tag t ON t.ambiance = a.id
                 LEFT JOIN c9.user u ON a.designer = u.id
@@ -33,6 +33,7 @@ class TagRepository extends EntityRepository
        $rsm->addScalarResult('photo', 'photo');
        $rsm->addScalarResult('pseudo', 'pseudo');
        $rsm->addScalarResult('prix', 'prix');
+       $rsm->addScalarResult('ville', 'viller');
 
         return $this->getEntityManager()
             ->createNativeQuery($sql, $rsm)
